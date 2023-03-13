f1d097ea8a7be760f03829fcde99a623b489905b
Eray YILMAZ <office@klienten-info.at>
2023-03-13 14:14:37 +0100

    dsd
    sd

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,12 @@ module.exports = {
       fontFamily: {
         sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'ci-color-1': '#a48544',
+        'ci-color-2': '#545a58',
+        'footer-bg' : '#545a58',
+        'ci-text-color:' : '#333333'
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
